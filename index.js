8288c921276b51a91434dbfc663b82095543ea65
Foster-J <jfoster67@my.bcit.ca>
2026-01-12 00:01:04 -0800

    added projects page route

--- index.js
+++ index.js
@@ -8,10 +8,16 @@ require('dotenv').config();
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 
+app.use(express.static('public'));
+
 const port = process.env.PORT || 3000;
 
-app.get('/',(req,res)=>{
+app.get('/', (req,res) => {
     res.render('index');
+});
+
+app.get('/projects', (req,res) => {
+    res.render('projects.ejs');
 });
 
 // 404 page
